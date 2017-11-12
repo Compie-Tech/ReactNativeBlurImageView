f9181a69189e436f6c7415a8f275a990b8419b61
Oron Meller <oron@compie.co.il>
2017-11-12 16:36:35 +0200

    * updated prop types to support react-native 0.50

--- blurimage.android.js
+++ blurimage.android.js
@@ -1,6 +1,6 @@
 var { requireNativeComponent, View } = require('react-native');
 
-var { PropTypes } = require('react')
+var { PropTypes } = require('prop-types')
 
 var iface = {
     name: 'BlurImageView',
